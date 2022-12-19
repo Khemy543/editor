401d547734cc6c1b09e934e9563a686bf7a73b38
Gideon Assafuah <gassafuah@gmail.com>
2022-12-19 21:40:03 +0000

    [IMP] initial push

---
+++ src/components/Layouts/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="fixed inset-x-0 h-20 py-3 px-4 max-w-7xl mx-auto flex justify-between bg-">
+      <a href="/">Test Editor</a>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
