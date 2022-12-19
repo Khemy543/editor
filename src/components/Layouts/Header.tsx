449e47445fdd0fb6bc2c7e321ae66a46fef18522
Gideon Assafuah <gassafuah@gmail.com>
2022-12-19 22:58:00 +0000

    [IMP] header and landing page

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,11 +1,149 @@
-import React from "react";
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+// import { Dialog } from "@headlessui/react";
+
+interface navigationItem {
+  name: string;
+  to: string;
+}
+
+const navigation: Array<navigationItem> = [
+  { name: "Contract", to: "#" },
+  { name: "Invoice", to: "#" },
+  { name: "Proposals", to: "#" },
+  { name: "Quote", to: "#" },
+];
 
 function Header() {
-  return (
-    <div className="fixed inset-x-0 h-20 py-3 px-4 max-w-7xl mx-auto flex justify-between bg-">
-      <a href="/">Test Editor</a>
+  const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
 
-      <div></div>
+  return (
+    <div className=" isolate bg-white">
+      <div className="absolute inse-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20ren]">
+        <svg
+          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+          viewBox="0 0 1155 678"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+            fillOpacity=".3"
+            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+          />
+          <defs>
+            <linearGradient
+              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+              x1="1155.49"
+              x2="-78.208"
+              y1=".177"
+              y2="474.645"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9089FC" />
+              <stop offset={1} stopColor="#FF80B5" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
+      <div className="px-6 pt-10 lg:px-8 container mx-auto">
+        <div>
+          <nav
+            className="flex h-9 items-center justify-between border-b border-gray-100 pb-10"
+            aria-label="Global"
+          >
+            <div className="flex lg:min-w-0 lg:flex-1">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className=" sr-only">True Editor</span>
+                True Editor{" "}
+                <span className="p-0.5 border-2 border-blue-600 rounded-md">
+                  TE
+                </span>
+              </a>
+            </div>
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
+              {navigation.map((item: navigationItem) => (
+                <a
+                  key={item.name}
+                  href={item.to}
+                  className="font-semibold text-gray-900 hover:text-gray-800"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
+              <a
+                href="#"
+                className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+              >
+                Create Document
+              </a>
+            </div>
+          </nav>
+          {/* <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+            <Dialog.Panel focus="true"
+              className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden"
+            >
+              <div className="flex h-9 items-center justify-between">
+                <div className="flex">
+                  <a href="#" className="-m-1.5 p-1.5">
+                    <span className="sr-only">Your Company</span>
+                    <img
+                      className="h-8"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      alt=""
+                    />
+                  </a>
+                </div>
+                <div className="flex">
+                  <button
+                    type="button"
+                    className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    onClick={() => setMobileMenuOpen(false)}
+                  >
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/10">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.to}
+                        className="-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                    >
+                      Log in
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Dialog> */}
+        </div>
+      </div>
     </div>
   );
 }
